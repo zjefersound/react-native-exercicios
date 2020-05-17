925562d4630c549aeb17ebbbf32a0e0279e6dac6
zjefersound <jeferson.2017317264@gmail.com>
2020-05-17 19:13:15 -0300

    Par impar com input real time

--- src/pages/ParImpar/index.js
+++ src/pages/ParImpar/index.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import { View, Button, Text, StatusBar } from 'react-native';
+import React, { Component, useState, useCallback } from 'react';
+import { View, Button, Text, TextInput, StatusBar } from 'react-native';
 
 //Estilos
 import styles from './styles';
@@ -9,14 +9,25 @@ import global from './../../styles/global';
 import NavBar from './../../components/NavBar';
 import ParImparFunc from './../../components/ParImparFunc';
 
+export const Entrada = props => {
+    return (
+        <TextInput style = { global.input }
+                onChangeText = { props.callback } 
+                value={ props.value } />
+    );
+}
 
 export default function ParImpar({ navigation }){
 
-    const num = 1;
+    const [ num, setNum ] = useState(0);
 
+    const atualizaTexto = (valor) => {
+        setNum(String(valor));
+    }
     return(
         <View style = { global.container }>
             <NavBar texto = 'ParImpar'/>
+            <Entrada value = { num } callback = { atualizaTexto } />
             <Text style = { global.title }>{ `O número ${num} é:` }</Text>
             <ParImparFunc numero = { num } />
         </View>
